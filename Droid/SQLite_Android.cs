c679ef20383ac184bf0aacef3950c5bcea4893a5
kevin@pichelman.com <kevin@pichelman.com>
2015-03-28 22:50:46 -0500

    Fixed Android database connection.

--- Droid/SQLite_Android.cs
+++ Droid/SQLite_Android.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using Todo;
 using Xamarin.Forms;
-using Todo.iOS;
+using TunnelSnakesPropertyManagement;
+using TunnelSnakesPropertyManagement.Droid;
 using System.IO;
 
 [assembly: Dependency (typeof (SQLite_Android))]
@@ -17,13 +17,46 @@ namespace TunnelSnakesPropertyManagement.Droid
 		#region ISQLite implementation
 		public SQLite.Net.SQLiteConnection GetConnection ()
 		{
-			var path = Forms.Context.Resources.OpenRawResource(Resource.Raw.TSPM);
+			var rawPath = System.Environment.GetFolderPath(Environment.SpecialFolder.Resources);
+			var sqliteFilename = "TSPM.db3";
+			string documentsPath = System.Environment.GetFolderPath (System.Environment.SpecialFolder.Personal); // Documents folder
+			var path = Path.Combine(documentsPath, sqliteFilename);
+
+			// This is where we copy in the prepopulated database
+			Console.WriteLine (path);
+			if (!File.Exists(path))
+			{
+				var s = Forms.Context.Resources.OpenRawResource(Resource.Raw.TSPM);  // RESOURCE NAME ###
+
+				// create a write stream
+				FileStream writeStream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write);
+				// write to the stream
+				ReadWriteStream(s, writeStream);
+			}
+
 			var plat = new SQLite.Net.Platform.XamarinAndroid.SQLitePlatformAndroid();
 			var conn = new SQLite.Net.SQLiteConnection(plat, path);
 
-			// Return the database connection 
 			return conn;
 		}
 		#endregion
+
+		/// <summary>
+		/// helper method to get the database out of /raw/ and into the user filesystem
+		/// </summary>
+		void ReadWriteStream(Stream readStream, Stream writeStream)
+		{
+			int Length = 256;
+			Byte[] buffer = new Byte[Length];
+			int bytesRead = readStream.Read(buffer, 0, Length);
+			// write the required bytes
+			while (bytesRead > 0)
+			{
+				writeStream.Write(buffer, 0, bytesRead);
+				bytesRead = readStream.Read(buffer, 0, Length);
+			}
+			readStream.Close();
+			writeStream.Close();
+		}
 	}
 }
